51c3bb55012b421b49c652ad74f16c5e1636eedd
parthmerchant <parth.merchant@rokt.com>
2022-03-13 18:00:46 -0400

    return type StreamingResponse update

--- src/app.py
+++ src/app.py
@@ -1,43 +1,34 @@
 from typing import List
 from fastapi import FastAPI
-from starlette.responses import FileResponse
+from starlette.responses import StreamingResponse
 from weasyprint import HTML
 from jinja2 import Environment, FileSystemLoader
+import io
 
 
-def _generate_template(filename):
-    template_loader = FileSystemLoader(searchpath="./")
-    template_env = Environment(loader=template_loader)
-    template = template_env.get_template(filename)
+def generate_template(filename):
+    env = Environment(loader=FileSystemLoader("."))
+    template = env.get_template(filename)
     return template
-    
 
-items = [
-    {
-        "column_1": "Hello",
-        "column_2": "World"
-    },
-    {
-        "column_1": "Hello",
-        "column_2": "World"
-    }
-]
+
+items = [{"column_1": "Hello", "column_2": "World"}, {"column_1": "Hello", "column_2": "World"}]
 
 name = "Parth"
 
-template = _generate_template("pdf_template.html")
+template = generate_template("pdf_template.html")
 
-html_out = template.render(name=name,items=items)
+html_out = template.render(name=name, items=items)
 
 html_byte_array = HTML(string=html_out).write_pdf()
 
 app = FastAPI()
 
-@app.get("/")
-def get_pdf():
 
-    return FileResponse(
-        html_byte_array,
+@app.get("/")
+async def get_pdf():
+    return StreamingResponse(
+        io.BytesIO(html_byte_array),
         media_type="application/pdf",
-        filename="ticket.pdf"
+        headers={"Content-Disposition": "attachment;filename=export.pdf"},
     )
