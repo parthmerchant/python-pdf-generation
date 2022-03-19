93e8b4d25e9a49db5e442efceb2defe4d0303748
parthmerchant <parth.merchant@rokt.com>
2022-03-18 21:12:15 -0400

    organize files

---
+++ src/pdf.py
@@ -0,0 +1,25 @@
+from weasyprint import HTML
+from jinja2 import FileSystemLoader, Environment
+from typing import Any
+
+items = [
+    {"column_1": "Hello", "column_2": "World"},
+    {"column_1": "Hello", "column_2": "World"},
+    {"column_1": "Hello", "column_2": "World"},
+    {"column_1": "Hello", "column_2": "World"},
+    {"column_1": "Hello", "column_2": "World"},
+    {"column_1": "Hello", "column_2": "World"},
+]
+
+name = 'Parth'
+
+def _generate_template(filename):
+    env = Environment(loader=FileSystemLoader("."))
+    template = env.get_template(filename)
+    return template
+
+def generate_pdf(template_filepath: str):
+        template = _generate_template(template_filepath)
+        html_out = template.render(name=name, items=items)
+        html_byte_array = HTML(string=html_out).write_pdf(stylesheets=["pdf.css"])
+        return html_byte_array
